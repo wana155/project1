05d713c9cb10dc994c08b86867604ef0bc33feb5
wana155 <apere129@ucr.edu>
2020-10-30 10:43:24 -0500

    Made blogs div scrollable using CSS stylings

--- javaScript/article.js
+++ javaScript/article.js
@@ -9,7 +9,7 @@ this.category = 'Technology';
 var dBTexto= new FileReader();
 //dBTexto.readAsText("../database.txt");
 //console.log("TEST XD");
-var articulos = new Array(3);
+var articulos = new Array(8);
 articulos[0]=new articles();
 articulos[0].author="Carlos Degollado";articulos[0].title="The Company Reaches 1M Views In Social Media";
 articulos[0].category="Social";
@@ -18,7 +18,22 @@ articulos[1].author="Kiana Billingslea";articulos[1].title="The Best Project Mil
 articulos[1].category="Inovation";
 articulos[2]=new articles();
 articulos[2].author="Daniel Barrera";articulos[2].title="5G Is Here! The Company (TM) Prepares For Inovation.";
-articulos[0].category="Technology";
+articulos[2].category="Technology";
+articulos[3]=new articles();
+articulos[3].author="Daniel Barrera";articulos[3].title="California Prop 22 Vote Heralds Judgment Day for Uber, DoorDash.";
+articulos[3].category="Politics";
+articulos[4]=new articles();
+articulos[4].author="Daniel Barrera";articulos[4].title="Apple stock pulled down by weak iPhone sales, China.";
+articulos[4].category="Business";
+articulos[5]=new articles();
+articulos[5].author="Daniel Barrera";articulos[5].title="Amazon Earnings, Revenue Beat in Q3 on North America Sales.";
+articulos[5].category="Business";
+articulos[6]=new articles();
+articulos[6].author="Daniel Barrera";articulos[6].title="Big Tech Profits Climb as Ad Sales Pick Up: Live Updates.";
+articulos[6].category="Social";
+articulos[7]=new articles();
+articulos[7].author="Daniel Barrera";articulos[7].title="Here are the highest-paying computer tech jobs in Milwaukee.";
+articulos[7].category="Technology";
 
 //Load articles in
 for(i=0;i<articulos.length;i++){
@@ -51,4 +66,120 @@ for(i=0;i<articulos.length;i++){
     forme.appendChild(conte);
     document.getElementById('cuerpo').appendChild(forme);
     document.getElementById('cuerpo').appendChild(brea);
+}
+function addElem(elem){
+
+        var forme = document.createElement('div');
+        forme.setAttribute("class","forma");
+        //forme.innerText="XKXK";
+    
+        var wrapperDive = document.createElement('div');
+        wrapperDive.setAttribute("class","wrapperDiv");
+    
+        var leftte = document.createElement('div');
+        leftte.innerText="By: "+elem.author;
+        leftte.setAttribute("class","leftt");
+    
+        var rightte = document.createElement('div');
+        rightte.setAttribute("class","rightt");
+        rightte.innerText="Category: "+elem.category;
+        wrapperDive.appendChild(leftte);
+        wrapperDive.appendChild(rightte);
+    
+        var hdos = document.createElement('h2');
+        hdos.innerText=elem.title;
+    
+        var conte = document.createElement('p');
+        conte.innerText=elem.content.substr(0,348)+"...";
+        var brea = document.createElement('br');
+        
+        forme.appendChild(wrapperDive);
+        forme.appendChild(hdos);
+        forme.appendChild(conte);
+        document.getElementById('cuerpo').appendChild(forme);
+        document.getElementById('cuerpo').appendChild(brea);
+}
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+//const container = document.querySelector('#container');
+//removeAllChildNodes(container);
+
+function categories(type){
+   var contenedores=document.getElementById("contenedores");
+   var children = contenedores.children;
+for (var i = 0; i < children.length; i++) {
+  var Child = children[i];
+    Child.setAttribute("class","list-group-item list-group-item-action");
+  // Do stuff
+  console.log(Child);
+}
+
+    if(type=="all"){
+        var parent = document.getElementById("cuerpo");
+        removeAllChildNodes(parent);
+        for(i=0;i<articulos.length;i++){
+                addElem(articulos[i]);}
+        var cate=document.getElementById("all");
+        cate.setAttribute("class","list-group-item list-group-item-action active");
+    }
+    else if (type=="busi"){
+        var parent = document.getElementById("cuerpo");
+        removeAllChildNodes(parent);
+        for(i=0;i<articulos.length;i++){
+            if (articulos[i].category=="Business")
+            { addElem(articulos[i]);}
+        }
+        var cate=document.getElementById("bus");
+        cate.setAttribute("class","list-group-item list-group-item-action active");
+    }
+    else if (type=="ino"){
+        var parent = document.getElementById("cuerpo");
+        removeAllChildNodes(parent);
+        for(i=0;i<articulos.length;i++){
+            if (articulos[i].category=="Inovation")
+            { addElem(articulos[i]);}
+        }
+        var cate=document.getElementById("ino");
+        cate.setAttribute("class","list-group-item list-group-item-action active");
+    }
+    else if (type=="pol"){
+        var parent = document.getElementById("cuerpo");
+        removeAllChildNodes(parent);
+        for(i=0;i<articulos.length;i++){
+            if (articulos[i].category=="Politics")
+            { addElem(articulos[i]);}
+        }
+        var cate=document.getElementById("pol");
+        cate.setAttribute("class","list-group-item list-group-item-action active");
+    }
+    else if (type=="soc"){
+        var parent = document.getElementById("cuerpo");
+        removeAllChildNodes(parent);
+        for(i=0;i<articulos.length;i++){
+            if (articulos[i].category=="Social")
+            { addElem(articulos[i]);}
+        }
+        var cate=document.getElementById("soc");
+        cate.setAttribute("class","list-group-item list-group-item-action active");
+    }
+    else if (type=="tec"){
+        var parent = document.getElementById("cuerpo");
+        removeAllChildNodes(parent);
+        for(i=0;i<articulos.length;i++){
+            if (articulos[i].category=="Technology")
+            { addElem(articulos[i]);}
+        }
+        var cate=document.getElementById("tec");
+        cate.setAttribute("class","list-group-item list-group-item-action active");
+    }
+    else if (type=="busi"){
+        var parent = document.getElementById("cuerpo");
+        removeAllChildNodes(parent);
+    }
+    else{alert("no pos wow");}
 }
